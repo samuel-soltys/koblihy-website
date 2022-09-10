55ae74044ad886257c15b829579f107fdcfebd90
Samuel Šoltys <samuelsasanka@gmail.com>
2022-09-10 23:02:47 +0200

    Small fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import { useEffect, useRef } from "react";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
     const donut = useRef<HTMLImageElement>(null);
