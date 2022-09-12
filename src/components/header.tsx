8ea0e2afbccd4bb154e5e5e19f0eee4391ea9bbf
Samuel Šoltys <samuelsasanka@gmail.com>
2022-09-12 16:10:09 +0200

    Header animation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,37 @@
-const Header = (() => {
-    return(
-    <div className="header">
-        <img src="logo.svg" className="logo" />
-        <div className="links">
-            <p>Team</p>
-            <p>Games</p>
-            <p>Competition</p>
-            <p>Contact</p>
+import { useState, useEffect } from "react";
+
+const Header = () => {
+    const [position, setPosition] = useState(0);
+    const [visible, setVisible] = useState(false);
+    
+    useEffect(() => {
+        const handleScroll = () => {
+            let moving = window.pageYOffset;
+            if (position > moving || position < 10) {
+                setVisible(true);
+            } else {
+                setVisible(false);
+            }
+            setPosition(moving);
+        };
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    });
+    const cls = visible ? "visible" : "hidden";
+    
+    return (
+        <div className={"header " + cls}>
+            <img src="logo.svg" className="logo" />
+            <div className="links">
+                <p>Team</p>
+                <p>Games</p>
+                <p>Competition</p>
+                <p>Contact</p>
+            </div>
         </div>
-    </div>
     );
-})
+};
 
 export default Header;
