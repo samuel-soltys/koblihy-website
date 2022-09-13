1a0112c46e241c2e19288b09ec77660575269b5f
Samuel Šoltys <samuelsasanka@gmail.com>
2022-09-13 20:39:02 +0200

    Scroll to top implementation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import "../scss/main.scss";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
     return <Component {...pageProps} />;
 }
 
