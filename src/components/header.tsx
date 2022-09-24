11cf0ca31b60c3533bb7f71e07f3429e6cd5ddba
Samuel Šoltys <samuelsasanka@gmail.com>
2022-09-24 21:49:33 +0200

    Fix mobile header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,11 @@ import useWindowDimensions from "../useWindowDimenstions";
 const Header = () => {
     const [position, setPosition] = useState(0);
     const [visible, setVisible] = useState(false);
+    const [menu, setMenu] = useState(false);
+    const [loadingFinished, setloadingFinished] = useState(false);
+    const cls = visible ? "visible" : "hidden";
 
+    // Hiding desktop menu on scroll
     useEffect(() => {
         const handleScroll = () => {
             let moving = window.pageYOffset;
@@ -25,21 +29,39 @@ const Header = () => {
             window.removeEventListener("scroll", handleScroll);
         };
     });
-    const cls = visible ? "visible" : "hidden";
-    if(typeof window !== "undefined") {
-        const { height, width } = useWindowDimensions();
-        useEffect(() => {
-            setTimeout(() => {
-                setVisible(true);
-                const mobileoverlay = document.getElementsByClassName("mobile-overlay")[0] as HTMLDivElement;
-                if (width < 1024) {
-                    mobileoverlay.style.display = "grid";
-                } else {
-                    mobileoverlay.style.display = "none";
-                }
-            }, 4300);
-        }, [])
-    }
+    // Turn off scroll for mobile menu
+    useEffect(() => {
+        if (loadingFinished) {
+            const html = document.querySelector("html");
+            if (menu) {
+                if (html) html.style.overflowY = "hidden";
+            } else {
+                if (html) html.style.overflowY = "scroll";
+            }
+        }
+    }, [menu]);
+
+    // Setting visibality after loading but 100ms before finishing
+    useEffect(() => {
+        setTimeout(() => {
+            setVisible(true);
+            setloadingFinished(true);
+        }, 4200);
+    }, []);
+    
+    // Hiding menu when resizing window after loading
+    useEffect(() => {
+        if (loadingFinished) {
+            const mobileoverlay = document.getElementsByClassName(
+                "mobile-overlay"
+            )[0] as HTMLDivElement;
+            if (visible && window.innerWidth < 1024) {
+                mobileoverlay.style.display = "grid";
+            } else {
+                mobileoverlay.style.display = "none";
+            }
+        }
+    });
 
     const HeaderLink = ({ id, label }: { id: string; label: string }) => {
         return (
@@ -56,18 +78,6 @@ const Header = () => {
             </Link>
         );
     };
-
-    const [menu, setMenu] = useState(false);
-
-    // Turn off scroll for mobile menu
-    useEffect(() => {
-        const html = document.querySelector("html");
-        if (menu) {
-            if (html) html.style.overflowY = "hidden";
-        } else {
-            if (html) html.style.overflowY = "scroll";
-        }
-    }, [menu]);
 
     return (
         <div className={"header " + cls}>
