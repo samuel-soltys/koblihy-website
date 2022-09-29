e8c5431715e622fcd93c1051877c4fc6a4ef3688
Samuel Šoltys <samuelsasanka@gmail.com>
2022-09-29 12:25:01 +0200

    Hotfix comments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,8 @@
 import "../scss/main.scss";
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
-// import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    // const { pathname } = useRouter();
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         window.scrollTo(0, 0);
-    //     }, 20);
-    // }, [pathname]);
     useEffect(() => {
         window.history.scrollRestoration = "manual";
     }, []);
