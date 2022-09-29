b30a92514cfeb6edcd3382ed0270efbb90067c73
Samuel Šoltys <samuelsasanka@gmail.com>
2022-09-29 12:19:41 +0200

    Window scroll to top fix 2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import "../scss/main.scss";
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    if (typeof window !== "undefined") {
-        window.scrollTo(0, 0);
-    }
+    const { pathname } = useRouter();
+    useEffect(() => {
+        setTimeout(() => {
+            window.scrollTo(0, 0);
+        }, 20);
+    }, [pathname]);
 
     return <Component {...pageProps} />;
 }
