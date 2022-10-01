8be7c2a2370bbf31a427a0729038fcf5eefbc386
Samuel Šoltys <samuelsasanka@gmail.com>
2022-10-01 19:37:06 +0200

    Language meta info

--- next.config.js
+++ next.config.js
@@ -3,5 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
+module.exports = {
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+};
 
 module.exports = nextConfig
