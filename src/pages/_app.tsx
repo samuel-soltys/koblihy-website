f1c1fba18650e52e0ed47471a02d88bc0a98fde6
Samuel Šoltys <samuelsasanka@gmail.com>
2022-10-01 19:23:26 +0200

    Revert "Google Tag"
    This reverts commit 9a6a42df3242204d978539a838e7211e6911ab0a.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,10 @@
 import "../scss/main.scss";
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
-import TagManager from "react-gtm-module";
-
 
 function MyApp({ Component, pageProps }: AppProps) {
     useEffect(() => {
         window.history.scrollRestoration = "manual";
-        TagManager.initialize({gtmId: "G-CVX2T07BGE"})
     }, []);
 
     return <Component {...pageProps} />;
